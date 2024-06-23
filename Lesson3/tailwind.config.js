1e0acf6f2af993ffa522996a45f811066578e3ce
Phú Nguyễn <117805903+PhuNguyen144@users.noreply.github.com>
2024-06-23 19:50:06 +0700

    Update tailwind.config.js

--- Lesson3/tailwind.config.js
+++ Lesson3/tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: ['./build/*.html'],
   theme: {
     extend: {
-      screen: {
+      screens: {
         'widescreen': { 'raw': '(min-aspect-ratio: 3/2' },
         'tallscreen': { 'raw': '(min-aspect-ratio: 1/2' },
       }
